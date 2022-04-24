a59805424ab525f7b36b6f6fa00c90da26c4391c
tlhbaris <103104761+tlhbaris@users.noreply.github.com>
2022-04-24 16:50:26 +0300

    operators

---
+++ Operators/Program.cs
@@ -0,0 +1,54 @@
+﻿// atama ve işlemli atama
+
+int x = 3;
+int y = 3;
+
+y = y + 2;
+y += 2;
+
+y /= 1;
+x *= 2;
+
+Console.WriteLine(y);
+Console.WriteLine(x);
+
+//mantıksal opratörler
+// ||, &&, ! 
+
+bool isSuccess = true;
+bool isCompleted = false;
+
+if (isSuccess && isCompleted)
+    Console.WriteLine("Perfect!");
+
+if (isSuccess || isCompleted)
+    Console.WriteLine("Great!");
+
+if(isSuccess && !isCompleted)
+    Console.WriteLine("Fine!");
+
+//ilişkisel operatörler
+//<, >, <=, >=, ==, !=
+
+int a = 3;
+int b = 4;
+bool sonuc = a < b;
+
+Console.WriteLine(sonuc);
+sonuc = a > b;
+Console.WriteLine(sonuc); 
+sonuc = a != b;
+Console.WriteLine(sonuc);
+
+//Aritmetik operatorler
+// /, *, +, -  
+// % : mod alır
+
+int sayi = 10;
+int sonuc1;
+sonuc1 = sayi++;
+Console.WriteLine(sayi);
+
+
+
+
